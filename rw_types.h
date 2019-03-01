ffa9c04db70ba98f896b0c1bd036cdc5b5402b71
Raymond Wan <rayxwan@gmail.com>
2019-03-01 14:20:31 -0500

    Change int typedefs

--- rw_types.h
+++ rw_types.h
@@ -13,18 +13,18 @@
 #endif
 #endif // #if !defined(RW_DISABLE_INTRINSICS)
 
-typedef int8_t i8;
-typedef int16_t i16;
-typedef int32_t i32;
-typedef int64_t i64;
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
 
-typedef uint8_t u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
 
-typedef float r32;
-typedef double r64;
+typedef float real32;
+typedef double real64;
 
 typedef intptr_t intptr;
 typedef uintptr_t uintptr;
